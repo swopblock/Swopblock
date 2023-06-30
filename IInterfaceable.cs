0e1c866edb8579bc3ae7f1dd77995f1d56d9738e
jeff <j.hilde@outlook.com>
2023-06-30 14:45:14 -0400

    working on interface hierarchy

---
+++ IInterfaceable.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace swop
+{
+    namespace Order
+    {
+        public interface IOrder { IOrder New(); }
+
+        public interface IOrderable { IOrdered Order(IOrder order); }
+
+        public interface IOrdered : IVerifiable{ }
+
+        public interface IVerifiable { IVerified Order(IOrder order); }
+
+        public interface IVerified : ISignable { }
+
+        public interface ISignable { ISigned Sign(IVerified order); }
+
+        public interface ISigned : ISubmittable { }
+
+        public interface ISubmittable { ISubmitted Submit(ISigned order); }
+
+        public interface ISubmitted : IConfirmable{ }
+
+        public interface IConfirmable { IConfirmed Confirm(ISubmitted order); }
+
+        public interface IConfirmed : IOrdered { }
+
+        namespace Charge //IAMHERE
+        {
+            public interface IExecutable { }
+
+            public interface IExecuted { }
+
+            namespace Offer
+            {
+                public interface IOfferable { }
+
+                public interface IOffered { }
+            }
+
+            namespace Invoice
+            {
+                public interface Invoiceable { }
+
+                public interface Invoiced { }
+            }
+
+        }
+
+        namespace Discharge
+        {
+            public interface IAdministratable { }
+
+            public interface IAdministated { }
+
+            namespace Deliver
+            {
+                public interface IDeliverable { }
+
+                public interface IDelivered { }
+            }
+
+            namespace Receipt
+            {
+                public interface IReceiptable { }
+
+                public interface IReceipted { }
+            }
+        }
+    }
+}
